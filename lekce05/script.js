a0cccae57de30fc45116ae9f2469922ef5469148
Magda Krejcirova <magda.krejcirova@sap.com>
2024-02-28 17:28:23 +0100

    lekce 05

--- lekce05/script.js
+++ lekce05/script.js
@@ -1,27 +1,18 @@
-const odstavec = document.querySelector("#ods2") // ovliveni ID v CSS
-const nadpis = document.querySelector(".nadpis")
-const vek = Number(prompt("Zadej vek: "))
-const popis = document.querySelector("#produkt p")
-const obrazek = document.querySelector("img")
+const produkt = document.querySelector("#produkt")
 
-obrazek.src = "https://www.pesweb.cz/img/_/_kocka.1813.foto/img_7774.jpg"
-obrazek.style.width = "300px"
-/* dalsi metody
-document.getElementById()
-document.getElementsByClassName()
-document.getElementsByTagName()
-*/
-if (vek >= 18) {
-    odstavec.textContent = "Vitej na strance"
-    odstavec.classList.add("zelena")
-    nadpis.style.color = "green"
-} else {
-    odstavec.textContent = "Pristup zamitnut"
-    odstavec.classList.add("cervena")
-    nadpis.style.color = "red"
-}    
+const kniha = {
+    nazev: "Kral krysa",
+    cena: 150,
+    popis: "Valecne drama"
+}
 
-console.log(odstavec)
-// dalsi methody
-// odstavec.classList.remove ("cervena") - odebirani tridy
-popis.textContent = "uzasny produkt pizza s anansem"
+//* produkt.className = " " /* vymze nama nastavenou clasu v index.html */
+
+produkt.classList.toggle("ramecek")
+
+produkt.innerHTML = `
+    <p class="nazev">${kniha.nazev}</p>
+    <p>${kniha.popis}</p>
+    <p>cena:${kniha.cena}</p>
+`
+
