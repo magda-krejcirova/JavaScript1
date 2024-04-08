62a22ae298f50ba33e61e6b4f748cc7cc0306cf1
Magda Krejcirova <magda.krejcirova@sap.com>
2024-04-08 12:22:08 +0200

    ukol 08 a 09

--- lekce10/homework1.js
+++ lekce10/homework1.js
@@ -25,56 +25,50 @@ numbers.forEach((polozka) => {
 })
 
 document.body.innerHTML += `<p> Vypište do stránky pouze sudá čísla: </p>`
+const sudy = numbers.map((cislo) => {
+  return (cislo % 2 === 0) ? document.body.innerHTML += `${cislo}, `: ""
+})
+
+document.body.innerHTML += `<p> Vypište do stránky pouze ta čísla, jejíchž absolutní hodnota je dělitelná třemi: </p>`
+const delit3 = numbers.map((cislo) => {
+  let abs = Math.abs(cislo)
+  return (abs % 3 === 0) ? document.body.innerHTML += `${cislo}, `: ""
+})
+
+document.body.innerHTML += `<p> Vypište do stránky jak daleko je každé číslo v seznamu od čísla 5: </p>`
+const vzdalenost = numbers.map((cislo) => {
+  let delka = Math.abs(cislo - 5)
+  return document.body.innerHTML += `${delka}, `
+})
+
+document.body.innerHTML += `<p> Vypište do stránky druhé mocnicny vzdáleností všech čísel od čísla 5: </p>`
+const vzdalenostCtverecni = numbers.map((cislo) => {
+  let delka = (cislo - 5) * (cislo - 5)
+  return document.body.innerHTML += `${delka}, `
+})
+
+let counter = 0
+document.body.innerHTML += `<p> Spočítejte, kolik je v seznamu záporných čísel: </p>`
 numbers.forEach((polozka) => {
-  const even = polozka => (polozka % 2 === 0) ? document.body.innerHTML +=`${even}, ` : " "
-  })
+  if (polozka < 0) {counter++}
+})
+document.body.innerHTML +=`${counter} `
 
+let sume = 0
+document.body.innerHTML += `<p> Spočítejte součet všech čísel v poli: </p>`
+numbers.forEach((polozka) => {
+  sume = sume + polozka
+})
+document.body.innerHTML +=`${sume} `
 
+document.body.innerHTML += `<p> Spočítejte průměr všech čísel v poli: </p>`
+let average = numbers.length
+average = sume / average
+document.body.innerHTML +=`${average} `
 
-// jmena.forEach((polozka) => { 
-//   document.body.innerHTML += `<p>${polozka}@czechitas.cz</p>`
-// })
-
-
-
-// const cisla = [7, 1, 4, 5, 2, 9, 3, 6, 8];
-// const zvirata = [
-//   'pes',
-//   'kočka',
-//   'králík',
-//   'had',
-//   'andulka',
-//   'morče',
-//   'krkavec',
-// ];
-// const napoje = [
-//   { nazev: 'Pivo', cena: 12, skladem: true },
-//   { nazev: 'Rum', cena: 120, skladem: false },
-//   { nazev: 'Víno', cena: 85, skladem: true },
-//   { nazev: 'Whisky', cena: 450, skladem: true },
-//   { nazev: 'Vodka', cena: 280, skladem: false },
-//   { nazev: 'Becherovka', cena: 210, skladem: true },
-//   { nazev: 'Kofola', cena: 40, skladem: true },
-//   { nazev: 'Voda', cena: 15, skladem: false },
-// ];
-
-// const dvojnasobek = cisla.map((cislo) => {
-//     return cislo * 2
-// })
-// console.log(dvojnasobek)
-
-// const delkyZvirat = zvirata.map((zvire) => {
-//     return zvire.length
-// })
-// console.log(delkyZvirat)
-
-// const jenNapoje = napoje.map((napoj) => {
-//     return napoj.nazev
-// })
-// console.log(jenNapoje)
-
-// const jeSkladem = napoje.map((sklad) => {
-//     if (sklad.skladem) {return sklad.nazev}
-//     else return "Neni skladem"
-// })
-// console.log(jeSkladem)
+let sumKlad = 0
+document.body.innerHTML += `<p> Spočítejte součet všech kladných čísel v poli: </p>`
+numbers.forEach((polozka) => {
+  if (polozka > 0) {sumKlad+=polozka}
+})
+document.body.innerHTML +=`${sumKlad} `
